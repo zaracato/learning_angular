fbf9852ec0176544f11923b5e3b7f9c9d150e14d
zaracato <zaracato@gmail.com>
2020-10-19 11:59:11 -0500

    inicial

---
+++ src/app/app.component.ts
@@ -0,0 +1,66 @@
+import { Component } from '@angular/core';
+import { IProduct } from './product.model';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'platzi';
+  items = ['nicolas', 'Arturo', 'Karen'];
+  name: string;
+
+  products: IProduct[] = [
+    {
+      id: '1',
+      image: 'assets/images/camiseta.png',
+      title: 'Camiseta',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '2',
+      image: 'assets/images/hoodie.png',
+      title: 'Hoodie',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '3',
+      image: 'assets/images/mug.png',
+      title: 'Mug',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '4',
+      image: 'assets/images/pin.png',
+      title: 'Pin',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '5',
+      image: 'assets/images/stickers1.png',
+      title: 'Stickers',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '6',
+      image: 'assets/images/stickers2.png',
+      title: 'Stickers',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    },
+  ];
+  addName(name: string){
+    this.items.push(name);
+  }
+
+  deleteName(index: number){
+    this.items.splice(index, 1);
+  }
+ 
+}
---
+++ src/app/product.model.ts
@@ -0,0 +1,9 @@
+
+
+export interface IProduct {
+    id: string;
+    title: string;
+    price: number;
+    description: string;
+    image: string;
+  }
